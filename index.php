2e5a85da5c5833cc75aab790068186478f68c485
diianita <dianitasar12@gmail.com>
2014-10-08 22:32:02 -0500

    agregados nuevos archivos

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include 'connection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php';?>
+    <body>
+
+        <?php include 'content.php';?>
+
+	<?php include 'footer.php';?>
+        <!--== Javascript Files ==-->
+        <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+        <script src="sources/js/jquery-2.1.0.min.js"></script>
+        <script src="sources/js/bootstrap.min.js"></script>
+        <script src="sources/js/jquery.scrollTo.js"></script>
+        <script src="sources/js/jquery.nav.js"></script>
+        <script src="sources/js/owl.carousel.min.js"></script>
+        <script src="sources/js/jquery.flexslider.js"></script>
+        <script src="sources/js/jquery.accordion.js"></script>
+        <script src="sources/js/jquery.placeholder.js"></script>
+        <script src="sources/js/jquery.fitvids.js"></script>
+        <script src="sources/js/gmap3.js"></script>
+        <script src="sources/js/fancySelect.js"></script>
+        <script src="sources/js/main.js"></script>
+
+    </body>
+</html>
