d2c674f96d2526a739315f990d9c44b8efe2500e
diianita <dianitasar12@gmail.com>
2014-10-14 18:13:22 -0500

    nuevos archivos

---
+++ view/admInformation.php
@@ -0,0 +1,67 @@
+<div class="content">
+    <div class="container box">
+        <section id="adm-information" class="row">
+            <div class="col-sm-12">
+                <div class="section-header text-center">
+                    <h2>Informaci&oacute;n de la instituci&oacute;n</h2>
+                </div>
+            </div>
+            <div class="col-sm-12">
+                <div class="row">
+                    <form class="form-horizontal" id="form-update-quienes" action="" method="post">
+                        <fieldset>
+                            <div class="form-group">
+                                <label class="col-md-2 control-label" for="message">Quienes Somos</label>
+                                <div class="col-md-9">
+                                    <textarea class="form-control" id="quienes" name="quienes" placeholder="" rows="5"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-11 text-right">
+                                    <button type="submit" class="btn btn-primary btn-lg">Actualizar</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+                <div class="row">
+                    <form class="form-horizontal" id="form-update-mision" action="" method="post">
+                        <fieldset>
+                            <div class="form-group">
+                                <label class="col-md-2 control-label" for="message">Misi&oacute;n</label>
+                                <div class="col-md-9">
+                                    <textarea class="form-control" id="mision" name="mision" placeholder="" rows="5"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-11 text-right">
+                                    <button type="submit" class="btn btn-primary btn-lg">Actualizar</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+                <div class="row">
+                    <form class="form-horizontal" id="form-update-vision" action="" method="post">
+                        <fieldset>
+                            <div class="form-group">
+                                <label class="col-md-2 control-label" for="message">Visi&oacute;n</label>
+                                <div class="col-md-9">
+                                    <textarea class="form-control" id="vision" name="vision" placeholder="" rows="5"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-11 text-right">
+                                    <button type="submit" class="btn btn-primary btn-lg">Actualizar</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </section>
+        <div id="gotop" class="gotop fa fa-arrow-up"></div>
+    </div>
+</div>
+
+
---
+++ view/admManagers.php
@@ -0,0 +1,47 @@
+<div class="content">
+    <div class="container box">
+        <section id="adm-information" class="row">
+            <div class="col-sm-12">
+                <div class="section-header text-center">
+                    <legend><h2>Administraci&oacute;n de Directivos</h2></legend>
+                </div>
+            </div>
+            <div class="col-sm-12">
+                <div class="container-1">
+                    <div class="col-md-2 well" style="margin: 4px;">
+                        <center>
+                            <form id="imageform-1" method="post" enctype="multipart/form-data" action="">
+                                <div class="form-group">
+                                    <div id="preview-image-1">
+                                        <img src="/wp-content/uploads/140778984031122.jpg" name="img-4" class="img-rounded" width="140" height="140" style="width: 140px;height: 140px;">
+                                    </div>
+                                    <a class="btn btn-primary" data-id="1" style="position: absolute;margin-left: -40px;margin-top: -42px;">Modificar
+                                        <input type="file" class="file-upload" data-id="1" name="photoimg-1" id="photoimg-1" style="opacity: 0;width: 85px;position: absolute;margin-left: -45px;margin-top: -29px;">
+                                    </a>
+                                </div>
+                                <div class="form-group">
+                                    <label for="nombre-1">Nombre</label>
+                                    <input type="text" class="form-control" id="nombre-1">
+                                </div>
+                                <div class="form-group">
+                                    <label for="nombre-1">Apellido</label>
+                                    <input type="text" class="form-control" id="apellido-1">
+                                </div>
+                                <div class="form-group">
+                                    <label for="nombre-1">Cargo</label>
+                                    <input type="text" class="form-control" id="cargo-1">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary">Actualizar</button>
+                                </div>
+                            </form>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div id="gotop" class="gotop fa fa-arrow-up"></div>
+    </div>
+</div>
+
+
