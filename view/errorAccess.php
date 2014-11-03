7b6c4820d38388931dee372e21678d3382133821
diianita <dianitasar12@gmail.com>
2014-11-03 13:20:17 -0500

    permisos de navegación

---
+++ view/errorAccess.php
@@ -0,0 +1,13 @@
+<div class="content">
+    <div class="container box">
+        <section class="row error-access">
+            <div class="col-sm-12">
+                <div class="section-header text-center">
+                    <h2>Debe iniciar sesi&oacute;n para poder acceder a esta vista</h2>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+
+
