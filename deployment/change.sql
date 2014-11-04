e261c02b83c1e5cdffda8e29f3f3f5525baaf21d
diianita <dianitasar12@gmail.com>
2014-11-04 07:04:55 -0500

    cambios a la base de datos

---
+++ deployment/change.sql
@@ -0,0 +1,6 @@
+INSERT INTO  `viisa`.`TipoUsuario` (`id` ,`desc` ,`enabled`)
+VALUES (NULL ,  'administrador',  '1');
+
+
+INSERT INTO  `viisa`.`Usuario` (`tipoUsuario` ,`id` ,`contrasena` ,`nombre` ,`apellido` ,`email` ,`foto` ,`enabled`) VALUES (
+'6', NULL ,  'admin123456',  'diana',  'sarmiento',  'diana.sarmiento@mail.com', NULL ,  '1');
